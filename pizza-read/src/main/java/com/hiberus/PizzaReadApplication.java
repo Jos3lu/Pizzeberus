0853e3c8856eebf57f2606ca6a4899d6174e674b
Jos3lu <jlsierrabenito@gmail.com>
2023-09-22 23:45:15 +0200

    Modified Spring Boot & Cloud version

--- pizza-read/src/main/java/com/hiberus/PizzaReadApplication.java
+++ pizza-read/src/main/java/com/hiberus/PizzaReadApplication.java
@@ -1,13 +1,14 @@
 package com.hiberus;
 
-/**
- * Hello world!
- *
- */
-public class PizzaReadApplication
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaClient
+public class PizzaReadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaReadApplication.class, args);
     }
 }
---
+++ pizza-read/src/main/java/com/hiberus/controllers/PizzaReadController.java
@@ -0,0 +1,17 @@
+package com.hiberus.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class PizzaReadController {
+
+    @GetMapping(value = "/pizzas/{pizzaId}")
+    public ResponseEntity<Void> getPizzas() {
+        return ResponseEntity.ok(null);
+    }
+
+}
