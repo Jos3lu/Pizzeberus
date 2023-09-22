617f3707acf34b941ef46aafdddd1a1e5794ad4e
Jos3lu <jlsierrabenito@gmail.com>
2023-09-22 17:54:57 +0200

    Added pizza-read, pizza-write & user module

---
+++ pizza-write/src/main/java/com/hiberus/PizzaWriteApplication.java
@@ -0,0 +1,13 @@
+package com.hiberus;
+
+/**
+ * Hello world!
+ *
+ */
+public class PizzaWriteApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
