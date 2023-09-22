da2df465a78662d967f9bcf93c3fa84bbb362440
Jos3lu <jlsierrabenito@gmail.com>
2023-09-22 16:56:24 +0200

    Updated in gateway spring cloud version

--- gateway-server/src/main/java/com/hiberus/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/hiberus/GatewayServerApplication.java
@@ -2,10 +2,9 @@ package com.hiberus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-@EnableEurekaClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GatewayServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class, args);
