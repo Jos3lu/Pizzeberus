7f22ccb8e6d8b207d7e3473a47de50e8715ef2a8
Jos3lu <jlsierrabenito@gmail.com>
2023-09-22 16:38:35 +0200

    Removes h2 dependency

--- config-server/src/main/java/com/hiberus/ConfigServerApplication.java
+++ config-server/src/main/java/com/hiberus/ConfigServerApplication.java
@@ -3,10 +3,11 @@ package com.hiberus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
